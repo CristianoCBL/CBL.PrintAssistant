af7ce98ba486d24b31992a5e2789f0ebc7d8ad23
CristianoCBL <contato@kbine.com.br>
2026-07-17 18:13:43 -0300

    Adiciona resolvedor de configuracoes por job

---
+++ CBL.PrintAssistant/EffectivePrintSettings.cs
@@ -0,0 +1,16 @@
+namespace CBL.PrintAssistant
+{
+    public sealed class EffectivePrintSettings
+    {
+        public string PrinterName { get; init; } = "";
+        public string PaperName { get; init; } = "";
+        public string Orientation { get; init; } = "Automático";
+        public string RotationMode { get; init; } = "Automático";
+        public string FitMode { get; init; } = "Cover";
+        public int Dpi { get; init; } = 300;
+        public int Bleed { get; init; }
+        public int MarginLeft { get; init; }
+        public int MarginTop { get; init; }
+        public int MarginRight { get; init; }
+        public int MarginBottom { get; init; }
+        public int OffsetX { get
